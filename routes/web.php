871b28e381b7bba51caa76b6cae4f25d965c72a6
Mustajab Ahmed <mustajabahmed02@gmail.com>
2024-10-03 17:03:22 +0500

    done the reverb

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,20 @@
 <?php
 
+use App\Events\SystemMaintenanceEvent;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+
+
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('testing', function () {
+    event(new SystemMaintenanceEvent(now()->toDateTimeString()));
+    return 'System Maintenance Event Triggered';
 });
